9ee83c557056ed45ffd6b663fee92242af39dd34
Gauthier Cler <gauthier.cler@epitech.eu>
2020-02-16 19:53:10 +0100

    add targeted attack

--- attack.py
+++ attack.py
@@ -7,44 +7,57 @@ import torchvision.transforms as transforms
 
 from classifier import ConvNet
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('--test-size', type=int, default=100, help='size of the test size')
-argparser.add_argument('--epsilon', type=float, default=0.1, help='epsilon value')
+argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+argparser.add_argument('--test-size', type=int, default=1000, help='number of sample evaluated')
+argparser.add_argument('--epsilon', type=float, default=0.05, help='epsilon value')
+argparser.add_argument('--target-value', type=int, default=0, choices=range(10), help='Targeted class')
+argparser.add_argument('--targeted', action='store_true', help='Enable class target on fgsm')
 
 args = argparser.parse_args()
 
 
-def evaluate(model, loader, epsilon):
-    correct = 0
-    criterion = nn.CrossEntropyLoss()
+class AdversarialAttack():
+    def __init__(self, model, loader, criterion, targeted, target_value):
+        self.model = model
+        self.loader = loader
+        self.criterion = criterion
+        self.targeted = targeted
+        self.target = torch.tensor([target_value])
 
-    for idx, data in enumerate(loader):
-        X, y = data
+    def fgsm(self, X, y, outputs, e):
+        loss = self.criterion(outputs, self.target if self.targeted else y)
+        if self.targeted:
+            loss *= -1
+        self.model.zero_grad()
+        loss.backward()
 
-        X.requires_grad = True
-        outputs = model(X)
+        grad = X.grad.data
+        X_adv = X + e * grad.sign()
 
-        _, predicted = torch.max(outputs.data, 1)
-        if predicted == y:
-            loss = criterion(outputs, y)
-            model.zero_grad()
+        return torch.clamp(X_adv, 0, 1)
 
-            loss.backward()
+    def evaluate(self, epsilon):
+        correct = 0
 
-            data_grad = X.grad.data
-            attack_image = fgsm(X, data_grad, e=epsilon)
+        for idx, data in enumerate(self.loader):
+            X, y = data
 
-            outputs = model(attack_image)
-            _, predicted_ = torch.max(outputs.data, 1)
+            X.requires_grad = True
+            # y.require_grad = True
 
-            if predicted == predicted_:
-                correct += 1
-    print(correct / len(loader))
+            outputs = self.model(X)
 
+            _, y_pred = torch.max(outputs.data, 1)
 
-def fgsm(input_image, grad, e=0.05):
-    new_image = e * grad.sign() + input_image
-    return torch.clamp(new_image, 0, 1)
+            if y_pred == y:
+                attack_image = self.fgsm(X, y, outputs, epsilon)
+
+                outputs = self.model(attack_image)
+                _, y_pred_ = torch.max(outputs.data, 1)
+
+                if y_pred == y_pred_:
+                    correct += 1
+        print(correct / len(self.loader))
 
 
 def main():
@@ -59,7 +72,11 @@ def main():
     subset = torch.utils.data.Subset(testset, indices=range(args.test_size))
     loader = torch.utils.data.DataLoader(subset,
                                          batch_size=1)
-    evaluate(model, loader, args.epsilon)
+
+    adv = AdversarialAttack(model, loader, criterion=nn.CrossEntropyLoss(),
+                            targeted=args.targeted, target_value=args.target_value)
+
+    adv.evaluate(args.epsilon)
 
 
 if __name__ == '__main__':
